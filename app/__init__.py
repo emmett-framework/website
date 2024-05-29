95653528fb8cc0f44fd6fe62ae841334b5240f31
Giovanni Barillari <g@baro.dev>
2024-05-30 01:12:58 +0200

    Update sentry config

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,12 @@ app.config_from_yaml('version.yml', 'emmett_src')
 app.config.url_default_namespace = "main"
 app.config.private_hostname = os.environ.get('PRIVATE_HOSTNAME', 'localhost')
 app.config.Haml.set_as_default = True
+app.config.Sentry.environment = os.environ.get('ENVIRONMENT', 'development')
 app.config.Sentry.dsn = os.environ.get('SENTRY_DSN', '')
+app.config.Sentry.release = os.environ.get('RELEASE', 'latest')
+app.config.Sentry.enable_tracing = os.environ.get('SENTRY_TRACING') == 'true'
+app.config.Sentry.tracing_sample_rate = 0.1
+app.config.Sentry.tracing_exclude_routes = ['private.health']
 
 app.use_extension(Haml)
 app.use_extension(Sentry)
